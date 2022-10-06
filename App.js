9cfff1e366a1e87c5ecbb7398e8c9b34931fdded
Goyo Regalado <goyoregalado@gmail.com>
2022-10-06 08:39:28 +0100

    Feature(App.js): Working on a product input code

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-import { StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-
+      <View style={styles.productInput}>
+        <TextInput style={styles.productName} placeholder='Introduzca un producto'/>
+        <Button style={styles.addButton}title="Añadir" />
+      </View>
+      <View>
+        <Text>Cuerpo</Text>
+      </View>
     </View>
   );
 }
@@ -11,8 +17,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  productInput: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: "#eef4ed",
+    width: '80%',
+    height: 80,
+    borderRadius: 5,
+    padding: 10
+  },
+  productName: {
+    flex: 4
+  },
+  addButton: {
+    flex: 1
+  }
+
 });

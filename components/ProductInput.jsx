257a75d6be5ceab573ef628ea545e0897c68fd19
cesar jesus leon martin <cesilius98@gmail.com>
2022-10-21 20:45:53 +0000

    Feature: hecho selector y su sistema de recogida de dato

--- components/ProductInput.jsx
+++ components/ProductInput.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-
+import SelectDropdown from 'react-native-select-dropdown'
 import { Button, StyleSheet, TextInput, View } from 'react-native';
-
+const countries = ["Egypt", "Canada", "Australia", "Ireland"]
 const ProductInput = ({ onProductAdd }) => {
     const [productName, setProductName] = useState({
         id:"",
@@ -20,10 +20,17 @@ const ProductInput = ({ onProductAdd }) => {
             }
         });
     }
-
+    const changeTypeHandler = (value) =>{
+        setProductName((productName)=>{
+            return{
+                ...productName,
+                type:value
+            }
+        })
+    }
     const addProductHandler = () => {
         
-        if (productName.name !== '') {
+        if (productName.name !== undefined) {
             onProductAdd(productName);
         }
         setProductName('');
@@ -31,29 +38,66 @@ const ProductInput = ({ onProductAdd }) => {
 
     return (
         <View style={styles.productInput}>
-            <TextInput style={styles.productName}
-                placeholder='Introduzca un producto'
-                keyboardType="default"
-                onChangeText={changeTextHandler}
-                value={productName} />
-            <Button
+            <View style={styles.productLine}>
+                <TextInput style={styles.productName}
+                    placeholder='Introduzca un producto'
+                    keyboardType="default"
+                    onChangeText={changeTextHandler}
+                    value={productName} />
+
+                <SelectDropdown
+                    buttonStyle={styles.select}
+                    defaultButtonText="Type"
+                    data={countries}
+                    onSelect={(selectedItem, index) => {
+                        changeTypeHandler(selectedItem)
+                    }}
+                    buttonTextAfterSelection={(selectedItem, index) => {
+                        // text represented after item is selected
+                        // if data array is an array of objects then return selectedItem.property to render after item is selected
+                        return selectedItem
+                    }}
+                    rowTextForSelection={(item, index) => {
+                        // text represented for each item in dropdown
+                        // if data array is an array of objects then return item.property to represent item in dropdown
+                        return item
+                    }}
+                />      
+            </View>
+            <View style={styles.secondLine}>
+                <Button
                 style={styles.addButton}
                 title="Añadir"
                 onPress={addProductHandler} />
+            </View>
+                
+            
+               
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     productInput: {
-        flexDirection: 'row',
+        flexDirection: 'column',
         justifyContent: 'space-around',
         alignItems: 'center',
         backgroundColor: "#ad1457",
         width: '80%',
-        height: 80,
+        height: 100,
         borderRadius: 5,
         padding: 10
+    },
+    productLine:{
+        flexDirection:"row",
+        height:"50%"
+    },
+    secondLine:{
+
+    },
+    select:{
+        height:50,
+        width:100
     },
     productName: {
         flex: 4,
